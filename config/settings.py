1b7d9f51809c11b6227a86df45363e2cd770a762
elJulioDev <alexisdevelopgz@gmail.com>
2026-04-10 18:15:19 -0400

    Ajuste en la variable de entorno.

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-esjmh1+)d784_vupxcp4gzh5j@0*!d3pq0%8d9t@#+ezr5aqy$'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
