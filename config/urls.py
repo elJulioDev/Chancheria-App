27f83b2fa32fc4f65a6654869f4da4ce798fe011
elJulioDev <alexisdevelopgz@gmail.com>
2026-04-12 12:43:09 -0400

    Se incluyo url acceso panel admin.

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     # Incluimos las URLs de la app 'gestion' en la raíz de la web
     path('', include('gestion.urls')), 
 ]
